c381617e560b59eaaa5673e50624c964dea023e9
awsl1414 <3030994569@qq.com>
2023-04-21 16:45:30 +0800

    ok

--- fastapi_new2/backend/scheams/user.py
+++
@@ -1,9 +0,0 @@
-
-
-from tortoise.contrib.pydantic import pydantic_model_creator
-from models import User
-
-
-User_Pydantic = pydantic_model_creator(User, name="User", exclude=["password"])
-UserIn_Pydantic = pydantic_model_creator(
-    User, name="UserIn", exclude_readonly=True)
--- fastapi_new3/backend/scheams/__init__.py
+++
@@ -1,3 +0,0 @@
-
-from .user import User_Pydantic, UserIn_Pydantic
-from .movie import Movie_Pydantic, MovieIn_Pydantic
